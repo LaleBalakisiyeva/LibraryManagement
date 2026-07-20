38fcb439b1e5f646371f5dfd23dc78bf3f6716f0
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-20 13:43:43 +0400

    feat(dal): configure User entity and map to AppDbContext with unique constraints

--- LibraryManagement.DAL/Context/AppDbContext.cs
+++ LibraryManagement.DAL/Context/AppDbContext.cs
@@ -19,6 +19,7 @@ namespace LibraryManagement.DAL.Context
       
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
