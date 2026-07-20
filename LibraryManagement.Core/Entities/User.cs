3c74317009bdb1a2d9f4142007f61ffcdf4bbe27
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-20 13:32:31 +0400

    feat(core): create User domain entity with password hash and role

---
+++ LibraryManagement.Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagement.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Role { get; set; } = "USER";
+        public bool IsActive { get; set; } = true;
+    }
+}
