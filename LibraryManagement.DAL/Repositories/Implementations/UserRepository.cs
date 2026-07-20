7d3d038e9fd9c9cd58eccba5ad934967f658fe0f
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-20 13:47:12 +0400

    feat(dal): implement UserRepository using EF Core

---
+++ LibraryManagement.DAL/Repositories/Implementations/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagement.Core.Entities;
+using LibraryManagement.DAL.Context;
+using LibraryManagement.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.DAL.Repositories.Implementations
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _dbSet.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _dbSet.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<bool> IsUsernameOrEmailExistsAsync(string username, string email)
+        {
+            return await _dbSet.AnyAsync(u => u.Username == username || u.Email == email);
+        }
+    }
+}
---
+++ LibraryManagement.DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.DAL.Repositories.Interfaces
+{
+    public interface IUserRepository : IGenericRepository<User>
+    {
+        Task<User?> GetByUsernameAsync(string username);
+        Task<User?> GetByEmailAsync(string email);
+        Task<bool> IsUsernameOrEmailExistsAsync(string username, string email);
+    }
+}
