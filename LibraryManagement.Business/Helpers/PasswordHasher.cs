c8e74deb0c16cf4f2036a473c30eb4d4c72f6b78
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-20 13:51:45 +0400

    feat(business): implement BCrypt password hashing and verification helper

---
+++ LibraryManagement.Business/Helpers/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.Helpers
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
