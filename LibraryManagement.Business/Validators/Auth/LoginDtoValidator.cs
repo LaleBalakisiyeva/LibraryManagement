450cd1d687c97ace0e095ef9757cd49730f6438f
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-21 12:50:13 +0400

    feat(business): add fluent validation rules for auth DTOs

---
+++ LibraryManagement.Business/Validators/Auth/LoginDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using LibraryManagement.Business.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.Validators.Auth
+{
+    public class LoginDtoValidator : AbstractValidator<LoginDto>
+    {
+        public LoginDtoValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.");
+        }
+    }
+}
---
+++ LibraryManagement.Business/Validators/Auth/RegisterDtoValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using LibraryManagement.Business.DTOs.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.Validators.Auth
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotEmpty().WithMessage("Username is required.")
+                .MinimumLength(3).WithMessage("Username must be at least 3 characters long.")
+                .MaximumLength(50).WithMessage("Username cannot exceed 50 characters.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("A valid email format is required.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+        }
+    }
+}
