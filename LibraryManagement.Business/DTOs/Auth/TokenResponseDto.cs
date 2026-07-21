e44a964018c07a16f1d71d28eaa6b7f85420ea16
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-21 12:35:07 +0400

    feat(business): add RegisterDto, LoginDto and TokenResponseDto models

---
+++ LibraryManagement.Business/DTOs/Auth/TokenResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.DTOs.Auth
+{
+    public class TokenResponseDto
+    {
+        public string Token { get; set; } = null!;
+        public DateTime Expiration { get; set; }
+    }
+}
