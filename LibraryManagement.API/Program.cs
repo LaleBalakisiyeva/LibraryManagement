3c7421cb9af7c298c2e60642d7a62f47004ae93b
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-07-30 15:23:20 +0400

    feat(api): implement custom JSON responses for auth errors
    - Overrode OnChallenge and OnForbidden events in JwtBearer setup.
    - Ensured 401 and 403 errors return informative and structured JSON messages.

--- LibraryManagement.API/Program.cs
+++ LibraryManagement.API/Program.cs
@@ -1,13 +1,14 @@
-using LibraryManagement.DAL;
-using LibraryManagement.Business;
 using FluentValidation;
-using LibraryManagement.Business.Validators.Book;
 using LibraryManagement.API.Middlewares;
+using LibraryManagement.Business;
+using LibraryManagement.Business.Validators.Book;
+using LibraryManagement.DAL;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Text;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -67,6 +68,26 @@ builder.Services.AddAuthentication(options =>
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecurityKey"]))
+    };
+
+   
+    options.Events = new JwtBearerEvents
+    {
+        OnChallenge = async context =>
+        {
+            context.HandleResponse();
+            context.Response.StatusCode = 401;
+            context.Response.ContentType = "application/json";
+            var result = JsonSerializer.Serialize(new { message = "You are not authorized to access this resource. Token is missing or invalid." });
+            await context.Response.WriteAsync(result);
+        },
+        OnForbidden = async context =>
+        {
+            context.Response.StatusCode = 403;
+            context.Response.ContentType = "application/json";
+            var result = JsonSerializer.Serialize(new { message = "You do not have permission to access this resource." });
+            await context.Response.WriteAsync(result);
+        }
     };
 });
 
