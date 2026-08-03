e689b7d25ee08ba166eb293ced05c2906380d763
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-08-03 12:19:15 +0400

    feat(order): implement OrderService with transaction management and rollback mechanism

---
+++ LibraryManagement.Business/Services/Implementations/OrderService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using LibraryManagement.Business.DTOs.Order;
+using LibraryManagement.Business.Services.Interfaces;
+using LibraryManagement.Core.Entities;
+using LibraryManagement.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public OrderService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<int> CreateOrderAsync(CreateOrderDto dto)
+        {
+            using var transaction=await _unitOfWork.BeginTransactionAsync();
+
+            try
+            {
+                var order = _mapper.Map<Order>(dto);
+
+                order.TotalAmount = dto.OrderItems.Sum(x => x.Quantity * x.UnitPrice);
+                order.OrderDate = DateTime.UtcNow;
+
+                await _unitOfWork.Orders.AddAsync(order);
+
+                await _unitOfWork.CommitAsync();
+
+                return order.Id;
+            }
+            catch
+            {
+                await _unitOfWork.RollbackAsync();
+                throw;
+            }
+        }
+
+        public async Task<int> CreateOrderWithFailureAsync(CreateOrderDto dto)
+        {
+            using var transaction=await _unitOfWork.BeginTransactionAsync();
+
+            try
+            {
+                var order=_mapper.Map<Order>(dto);
+                order.TotalAmount=dto.OrderItems.Sum(x=>x.Quantity * x.UnitPrice);
+                order.OrderDate = DateTime.UtcNow;
+
+                await _unitOfWork.Orders.AddAsync(order);
+
+                await _unitOfWork.SaveChangesAsync();
+
+                throw new InvalidOperationException("Simulated transaction failure for testing rollback!");
+            }
+            catch
+            {
+                await _unitOfWork.RollbackAsync();
+                throw;
+            }
+
+        }
+    }
+}
---
+++ LibraryManagement.Business/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,16 @@
+﻿using LibraryManagement.Business.DTOs.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<int> CreateOrderAsync(CreateOrderDto dto);
+
+        Task<int> CreateOrderWithFailureAsync(CreateOrderDto dto);
+    }
+}
