7dd01c53beab55b8160be913bfbed3eedabaa7b7
Lalə Balakişiyeva <lalebalakisiyeva258@gmail.com>
2026-08-03 11:17:14 +0400

    feat(order): add CreateOrderDto and CreateOrderItemDto for order creation

---
+++ LibraryManagement.Business/DTOs/Order/CreateOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using LibraryManagement.Business.DTOs.OrderItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.DTOs.Order
+{
+    public class CreateOrderDto
+    {
+        public int UserId { get; set; }
+        public List<CreateOrderItemDto> OrderItems { get; set; } = new List<CreateOrderItemDto>();
+    }
+}
---
+++ LibraryManagement.Business/DTOs/OrderItem/CreateOrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Business.DTOs.OrderItem
+{
+    public class CreateOrderItemDto
+    {
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
